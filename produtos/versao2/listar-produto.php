4ec74486756468321853069fdd636268f53e1112
Izabelle ferreira da silva <127629684+izabelle20@users.noreply.github.com>
2023-11-17 11:02:50 -0300

    Add files via upload

--- produtos/versao2/listar-produto.php
+++ produtos/versao2/listar-produto.php
@@ -1,27 +1,25 @@
 <?php
+// Configuração do cabeçalho para indicar que a resposta é em formato JSON
 header('Content-Type: application/json');
 
+// Conectar ao banco de dados (substitua os valores pelos seus)
 $con = mysqli_connect("localhost", "root", "", "cadastro_produtos");
 
 // Inicializar dados de resposta JSON
 $dados = array();
-$dados['erro'] = false;
-$dados['mensagem'] = "";
-$dados['produtos'] = array();
+$dados['erro'] = false; // Indica se houve algum erro durante a execução
+$dados['mensagem'] = ""; // Mensagem associada ao resultado da operação
+$dados['produtos'] = array(); // Array que conterá os dados dos produtos
 
-// Verificar se o parâmetro "id" está presente na URL
-if (isset($_GET['id'])) {
-    $produto_id = intval($_GET['id']);
+// Consultar todos os produtos na tabela "produtos"
+$sql = "SELECT * FROM produtos";
+$result = mysqli_query($con, $sql);
 
-    // Consultar produto com o ID fornecido
-    $sql = "SELECT * FROM produtos WHERE id = $produto_id";
-    $result = mysqli_query($con, $sql);
-
-    // Verificar se o produto foi encontrado
-    if (mysqli_num_rows($result) > 0) {
-        $produto = mysqli_fetch_assoc($result);
-
-        // Adicionar o produto aos dados de resposta
+// Verificar se há produtos retornados
+if (mysqli_num_rows($result) > 0) {
+    // Iterar sobre cada linha de resultado
+    while ($produto = mysqli_fetch_assoc($result)) {
+        // Adicionar cada produto aos dados de resposta
         $item = array(
             'id' => intval($produto['id']),
             'nome' => $produto['nome'],
@@ -29,15 +27,12 @@ if (isset($_GET['id'])) {
             'valor' => $produto['valor'],
             'status' => $produto['status']
         );
-
-        $dados['produtos'] = $item;
-    } else {
-        $dados['erro'] = true;
-        $dados['mensagem'] = "Nenhum produto encontrado com o ID $produto_id";
+        array_push($dados['produtos'], $item);
     }
 } else {
+    // Se nenhum produto foi encontrado, configurar dados de erro
     $dados['erro'] = true;
-    $dados['mensagem'] = "Parâmetro 'id' ausente na URL";
+    $dados['mensagem'] = "Nenhum produto encontrado";
 }
 
 // Retornar os dados como JSON
